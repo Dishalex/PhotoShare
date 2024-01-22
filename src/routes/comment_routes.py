84b8ff77b2e8d542358c3c6f4fe76dd8c4436b1c
Roman-K26 <romankuspys777@gmail.com>
2024-01-22 16:18:27 +0300

    commit 2

--- src/routes/comment_routes.py
+++ src/routes/comment_routes.py
@@ -14,6 +14,15 @@ router = APIRouter(prefix='/comments', tags=['comments'])
 async def create_comments(comment_data: CommentSchema, image_id: int,
                           current_user: User = Depends(get_current_user),
                           db: AsyncSession = Depends(get_db)):
+    """
+        Create a new comment and associate it with an image.
+
+        :param comment_data: Data for the new comment.
+        :param image_id: The ID of the image to which the comment is associated.
+        :param current_user: The user creating the comment.
+        :param db: The asynchronous database session.
+        :return: The created comment.
+    """
     try:
         created_comment = await create_comment(db, image_id, comment_data.comment, current_user)
         return created_comment
@@ -25,6 +34,15 @@ async def create_comments(comment_data: CommentSchema, image_id: int,
 @router.put("/{comment_id}/", response_model=CommentsResponse)
 async def update_comments(comment_id: int, comment: CommentSchema,
                           current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    """
+       Update an existing comment.
+
+       :param comment_id: The ID of the comment to be updated.
+       :param comment: The updated comment data.
+       :param current_user: The user updating the comment.
+       :param db: The asynchronous database session.
+       :return: The updated comment or raise HTTPException if the comment is not found or permission denied.
+    """
     updated_comment = await update_comment(db, comment_id, comment.comment, current_user)
 
     if updated_comment:
@@ -37,6 +55,14 @@ async def update_comments(comment_id: int, comment: CommentSchema,
 @router.delete("/{comment_id}/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_comments(comment_id: int, current_user: User = Depends(get_current_user),
                           db: AsyncSession = Depends(get_db)):
+    """
+        Delete a comment.
+
+        :param comment_id: The ID of the comment to be deleted.
+        :param current_user: The user attempting to delete the comment.
+        :param db: The asynchronous database session.
+        :raise HTTPException: If the comment is not found or permission denied.
+    """
     success = await delete_comment(db, comment_id, current_user)
 
     if not success:
